e4d2eabc5ea53de1ce73bfa317cb0344f775d5fd
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-17 23:12:15 +0100

    feat: base components and pages add

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { gql, useQuery } from "@apollo/client";
+import { Event } from "./pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-query{
-  lessons{
-    id
-    title
-  }
-}
-`;
-interface Lesson {
-  id: string,
-  title: string;
-  type: string;
-}
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  console.log(data)
+ 
   return (
 
-    <ul>Event Plataform
-      {
-        data?.lessons.map(lesson => {
-          return <li key={lesson.id}>{lesson.title}</li>
-        })
-      }
-    </ul>
+    <div>
+      <Event/>
+    </div>
   )
 }
 
