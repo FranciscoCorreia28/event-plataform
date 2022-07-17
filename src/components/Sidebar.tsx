d2aa7ac35a2975a876e676f64140be7312adf0cc
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-18 00:46:31 +0100

    chore: react icons library phosphor add

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,20 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar(){
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl border-b border-gray-500 mb-6 pb-6 block ">
                 Cronograma de Aulas
             </span>
+            <div className="flex flex-col gap-8">
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+            </div>
         </aside>
     )
 }
