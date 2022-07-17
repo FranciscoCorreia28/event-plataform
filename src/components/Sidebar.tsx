57ab6b96705364ffa76faa2cc1f07b31d3fab319
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-18 00:14:48 +0100

    feat: sidebar add

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,9 @@
 export function Sidebar(){
-    return <h1>Sidebar</h1>
+    return (
+        <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+            <span className="font-bold text-2xl border-b border-gray-500 mb-6 pb-6 block ">
+                Cronograma de Aulas
+            </span>
+        </aside>
+    )
 }
