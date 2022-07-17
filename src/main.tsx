80d4d2abf96f5c9d686290774c169467cc01b466
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-17 21:06:46 +0100

    feat: add apollo config, add provider to our main component, and data fetched from our api

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloProvider } from "@apollo/client";
+import {client} from "./lib/apollo"
 import "./assets/styles/global.css"
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
