b978aab626d35e73a87383fcb917423bd8203c3d
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-18 13:35:28 +0100

    feat: half part of home screen done

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 
 import { CheckCircle, Lock } from "phosphor-react";
-import { isPast } from "date-fns";
+import { isPast, format} from "date-fns";
+import ptPT from 'date-fns/esm/locale/pt';
 interface LessonProps {
     title: string;
     slug?: string;
@@ -9,10 +10,15 @@ interface LessonProps {
 }
 export function Lesson(props: LessonProps) {
     const isAvailable = isPast(props.availableAt);
+    const AvailableAtFormated = format(props.availableAt, "EEEE ' • ' d ' de ' MMMM ' • ' H'h'mm", {
+        locale:ptPT
+       })
     return (
         <a href="#">
             <span className="text-gray-300">
-                {props.availableAt.toString()}
+             {
+              AvailableAtFormated   
+             }
             </span>
             <div className="rounded border border-gray-500 p-4 mt-2">
                 <header className="flex items-center justify-between">
