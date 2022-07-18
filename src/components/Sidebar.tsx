7daeb8238c8a4b5085dadc34a4678974ebdea4ee
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-07-18 12:58:59 +0100

    feat: getting the lessons from our api

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,44 @@
 import { Lesson } from "./Lesson";
+import { gql, useQuery } from "@apollo/client";
 
+const GET_LESSON_QUERY = gql`
+    query  {
+        lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+            id
+            lessonType
+            title
+            slug
+            availableAt
+        }
+    }
+`;
+interface getLessonsResponseType {
+    lessons:{
+        id:string,
+        lessonType:"live"|"class"
+        title:string;
+        slug:string;
+        availableAt:string;
+    }[]
+}
 export function Sidebar(){
+const {data}= useQuery<getLessonsResponseType>(GET_LESSON_QUERY);
+    console.log(data);
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl border-b border-gray-500 mb-6 pb-6 block ">
                 Cronograma de Aulas
             </span>
             <div className="flex flex-col gap-8">
-                <Lesson 
-                availableAt={new Date("10-10-2022")} 
-                type="live" 
-                title="Abertura do evento Ignite Labs"  />
-                
+            {
+            data?.lessons.map(lesson =>{
+                return(
+                <Lesson
+                key={lesson.id} 
+                availableAt={new Date(lesson.availableAt)} 
+                type={lesson.lessonType} 
+                title={lesson.title}  />
+            )})}
             </div>
         </aside>
     )
